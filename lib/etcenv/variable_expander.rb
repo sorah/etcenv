b92ff33df384bf4617a071e63a879678542e2078
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-04-02 20:02:57 +0900

    Add Etcenv::VariableExpander

---
+++ lib/etcenv/variable_expander.rb
@@ -0,0 +1,129 @@
+module Etcenv
+  class VariableExpander
+    class LoopError < StandardError; end
+    class DepthLimitError < StandardError; end
+
+    def self.expand(variables, max = 10)
+      new(variables).expand(max)
+    end
+
+    def initialize(variables)
+      @variables = variables.dup.freeze
+    end
+
+    attr_reader :variables
+
+    def expand(max = 10)
+      detect_loop!
+
+      result = {}
+      solve_order(max).map do |x|
+        result[x] = single_expand(@variables[x], result)
+      end
+      result
+    end
+
+    def variable_with_deps
+      @variables.map { |k, v| [k, [v, detect_variables(v)]] }
+    end
+
+    def dependees_by_variable
+      @dependees_by_variable ||= variable_with_deps.inject({}) do |r, x|
+        k, v, deps = x[0], x[1][0], x[1][1]
+
+        deps.each do |dep|
+          r[dep] ||= []
+          r[dep] << k
+        end
+        r
+      end.freeze
+    end
+
+    def root_variables
+      @root_variables ||= variable_with_deps.inject([]) do |r, x|
+        k, v, deps = x[0], x[1][0], x[1][1]
+        if deps.empty?
+          r << k
+        end
+        r
+      end + (variables.keys - dependees_by_variable.keys)
+    end
+
+    def detect_loop!
+      if root_variables.empty?
+        raise LoopError, "there's no possible root variables (variables which don't have dependee)"
+      else
+        dependees_by_variable.each do |k, deps|
+          deps.each do |dep|
+            if (dependees_by_variable[dep] || []).include?(k)
+              raise LoopError, "There's a loop between $#{dep} and $#{k}"
+            end
+          end
+        end
+      end
+    end
+
+    def solve_order(max_depth = 10)
+      order = []
+      solve = nil
+      solve = ->(vs, depth = 0) do
+        raise DepthLimitError if depth.succ > max_depth
+        vs.each do |x|
+          order.concat solve.call(dependees_by_variable[x] || [], depth.succ) + [x]
+        end
+      end
+      solve.call(root_variables)
+
+      uniq_with_keeping_first_appearance(order).reverse
+    end
+
+    private
+
+    VARIABLE = /
+      (?<escape>\\?)
+      \$
+      (
+        {(?<name>[a-zA-Z0-9_]+)}
+      |
+        \g<name>
+      )
+    /x
+
+    def detect_variables(str)
+      result = []
+
+      pos = 0
+      while match = str.match(VARIABLE, pos)
+        pos = match.end(0)
+        next if match['escape'] == '\\'
+
+        result << match['name']
+      end
+
+      result
+    end
+
+    def single_expand(str, variables)
+      str.gsub(VARIABLE) do |variable|
+        match = $~
+
+        if match['escape'] == '\\'
+          variable[1..-1]
+        else
+          variables[match['name']].to_s
+        end
+      end
+    end
+
+    def uniq_with_keeping_first_appearance(array)
+      set = {}
+      result = []
+      array.each do |x|
+        next if set[x]
+        result.push x
+        set[x] = true
+      end
+      result
+    end
+  end
+end
---
+++ spec/variable_expander_spec.rb
@@ -0,0 +1,191 @@
+require 'spec_helper'
+require 'etcenv/variable_expander'
+
+describe Etcenv::VariableExpander do
+  let(:string) { '' }
+  let(:variables) { { 'VAR' => 'var', 'STR' => string } }
+  subject(:result) { described_class.expand(variables) }
+
+  describe "single expansion" do
+    subject { result['STR'] }
+
+    context "no variables" do
+      it { is_expected.to eq '' }
+    end
+
+    context "with variable" do
+      let(:string) { "$VAR" }
+
+      it { is_expected.to eq 'var' }
+    end
+
+    context "with {variable}" do
+      let(:string) { "${VAR}" }
+
+      it { is_expected.to eq 'var' }
+    end
+
+    context "with unexist variable" do
+      let(:string) { "$NOVAR" }
+
+      it { is_expected.to eq '' }
+    end
+
+    context "with string including variable" do
+      let(:string) { "foo $VAR baz" }
+
+      it { is_expected.to eq "foo var baz" }
+    end
+
+    context "with string including variable" do
+      let(:string) { "foo ${VAR} baz" }
+
+      it { is_expected.to eq "foo var baz" }
+    end
+
+    context "with multiple variable expansions" do
+      let(:string) { "${VAR}${VAR}" }
+
+      it { is_expected.to eq "varvar" }
+    end
+
+    context "with multiple variables expansions" do
+      let(:variables) { { 'VAR' => 'var', 'VAR2' => 'var2', 'STR' => string } }
+      let(:string) { "${VAR}${VAR2}" }
+
+      it { is_expected.to eq "varvar2" }
+    end
+
+    context "with multiple variables expansions" do
+      let(:variables) { { 'VAR' => 'var', 'VAR2' => 'var2', 'STR' => string } }
+      let(:string) { "${VAR}${VAR2}" }
+
+      it { is_expected.to eq "varvar2" }
+    end
+
+    context "with escaped variable" do
+      let(:string) { "\\$VAR" }
+
+      it { is_expected.to eq "$VAR" }
+    end
+
+    context "with escaped {variable}" do
+      let(:string) { "\\${VAR}" }
+
+      it { is_expected.to eq "${VAR}" }
+    end
+
+    context "with escaped variable and variable" do
+      let(:string) { "$VAR \\$VAR $VAR" }
+
+      it { is_expected.to eq "var $VAR var" }
+    end
+  end
+
+  describe "multiple expansions" do
+    let(:variables) do
+      {
+        'VAR2' => '${VAR}',
+        'VAR' => 'var'
+      }
+    end
+
+    it "resolves correctly" do
+      expect(subject['VAR']).to eq 'var'
+      expect(subject['VAR2']).to eq 'var'
+    end
+  end
+
+  describe "nested multiple expansions" do
+    # VALUE
+    #   VAR
+    #     FOO, BAZ
+    #       BAR
+    #     VAR2
+    let(:variables) do
+      {
+        'VAR2' => '${VAR}',
+        'BAR' => '${FOO} bar ${BAZ}',
+        'VAR' => '${VALUE}',
+        'FOO' => 'foo ${VAR}',
+        'BAZ' => 'baz ${VAR}',
+        'VALUE' => 'var'
+      }
+    end
+
+    it "resolves correctly" do
+      expect(result).to eq(
+        'VALUE' => 'var',
+        'VAR' => 'var',
+        'FOO' => 'foo var',
+        'BAZ' => 'baz var',
+        'BAR' => 'foo var bar baz var',
+        'VAR2' => 'var',
+      )
+    end
+  end
+
+  context "looped multiple expansions with root" do
+    # VAR
+    #   VAR2
+    let(:variables) do
+      {
+        'ROOT' => '${VAR}',
+        'VAR2' => '${VAR}',
+        'VAR' => '${VAR2}',
+      }
+    end
+
+    it "raises error" do
+      expect { subject }.to raise_error(Etcenv::VariableExpander::LoopError)
+    end
+  end
+
+
+  context "looped multiple expansions" do
+    # VAR
+    #   VAR2
+    let(:variables) do
+      {
+        'VAR2' => '${VAR}',
+        'VAR' => '${VAR2}',
+      }
+    end
+
+    it "raises error" do
+      expect { subject }.to raise_error(Etcenv::VariableExpander::LoopError)
+    end
+  end
+
+  context "nested looped multiple expansions" do
+    # VAR
+    #   VAR2
+    #     VAR3
+    let(:variables) do
+      {
+        'VAR' => '${VAR2}',
+        'VAR2' => '${VAR3}',
+        'VAR3' => '${VAR}',
+      }
+    end
+
+    it "raises error" do
+      expect { subject }.to raise_error(Etcenv::VariableExpander::LoopError)
+    end
+  end
+
+  context "exceed max_depth" do
+    # VAR
+    #   VAR2
+    #     VAR3
+    let(:variables) do
+      Hash[(1..10).map do |i|
+        ["VAR#{i}", (1...i).map { |_| "${VAR#{_}}" }.join]
+      end]
+    end
+
+    it "raises error" do
+      expect { subject }.to raise_error(Etcenv::VariableExpander::DepthLimitError)
+    end
+  end
+end
