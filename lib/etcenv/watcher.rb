241a8df82f4c53ef736e0bfafbee6c1a47472fc5
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-04-08 07:51:10 +0900

    Extract thread block to a method

--- lib/etcenv/watcher.rb
+++ lib/etcenv/watcher.rb
@@ -16,12 +16,7 @@ module Etcenv
     def watch
       ch = Queue.new
       threads = env.modified_indices.map do |key, index|
-        Thread.new do
-          $stderr.puts "[watcher] waiting for change on #{key} (index: #{index.succ})" if verbose
-          etcd.watch(key, recursive: true, index: index.succ)
-          $stderr.puts "[watcher] dir #{key} has updated" if verbose
-          ch << key
-        end
+        Thread.new(ch, key, index, &method(:watch_thread))
       end
       report = ch.pop
       threads.each(&:kill)
@@ -35,6 +30,15 @@ module Etcenv
         env.load
         yield env if block_given?
       end
+    end
+
+    private
+
+    def watch_thread(ch, key, index)
+      $stderr.puts "[watcher] waiting for change on #{key} (index: #{index.succ})" if verbose
+      etcd.watch(key, recursive: true, index: index.succ)
+      $stderr.puts "[watcher] dir #{key} has updated" if verbose
+      ch << key
     end
   end
 end
